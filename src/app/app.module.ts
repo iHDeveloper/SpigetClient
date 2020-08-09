2cd887d7b6831c4ca9a7b9cb1f29f9462eaeb109
iHDeveloper <20463031+iHDeveloper@users.noreply.github.com>
2020-08-09 19:08:23 +0300

    Implement updated resources in the home page
    - Improve the design of resource card
    - Split the resources section into new and updated
    - Add icon to the resource card
    - Add ripple to the resource card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatRippleModule } from '@angular/material/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -29,6 +30,7 @@ import { ResourceCardComponent } from './resource-card/resource-card.component';
     BrowserAnimationsModule,
     MatIconModule,
     MatButtonModule,
+    MatRippleModule,
     MatSidenavModule,
     MatProgressSpinnerModule,
     MatToolbarModule,
